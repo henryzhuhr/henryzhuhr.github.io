58812014faa1df28a3018a91e7ac4dd0c82d46a7
henryzhuhr <henryzhuhr@users.noreply.github.com>
2026-08-20 03:29:19 +0000

    deploy: henryzhuhr/workspace@c8921db25921c34777b576799cc723e365903590

---
+++ assets/chunks/swimlanesDiagram-VK2B7HYN.ClaUv2d7.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB.xPXN8_Ir.js";import{_ as a}from"../app.B9I6IHSj.js";import"./chunk-5VM5RSS4.1nzoUvtU.js";import"./chunk-XXDRQBXY.BqHtJBxL.js";import"./chunk-POPQ4Y6H.1oWaEdEO.js";import"./chunk-F27PBJKO.BHzgK3CI.js";import"./channel.6Rr90zI2.js";import"./framework.DmcojIVA.js";import"./theme.BMPJ4urX.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
